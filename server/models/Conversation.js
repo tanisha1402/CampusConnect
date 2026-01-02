547634c1fc8e9e216fe18e7711ae9df3f9102f56
tanisha1402 <tanishachowdhury1402@gmail.com>
2026-01-03 02:00:21 +0600

    fixed conversation 1 to 1 issue

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -17,7 +17,5 @@ const conversationSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// 🔒 Ensure 1-to-1 conversation uniqueness
-conversationSchema.index({ participants: 1 }, { unique: true });
 
 module.exports = mongoose.model("Conversation", conversationSchema);
